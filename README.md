01d3d1672d91b2dc5807dddaf468a8b04cafdb01
risherlock <risherlock221b@gmail.com>
2024-09-08 17:00:54 +0200

    print register configs in usable form

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 ### stm32f103
 
-1. [ina219](drivers/stm32f103/libs/ina219/) - Zerø-drift, bidirectional current/power monitor with I2C interface.
+1. [ina219](drivers/stm32f103/libs/ina219/README.md) - Zerø-drift, bidirectional current/power monitor with I2C interface.
+2. [si4463](drivers/stm32f103/libs/si446x/) - High performance, low-current transceiver
--- drivers/stm32f103/libs/si446x/si446x_regcal.cpp
+++ drivers/stm32f103/libs/si446x/si446x_regcal.cpp
@@ -11,6 +11,7 @@
  * 1. outdiv is not same in datasheet and .xlsx calculator.
  * 2. Modem data rate is different for rates more than 200 kbps.
  * 3. pa_clock_duty_cycle and pa_output_bias
+ * 4. Make sure to configure GPIO pins for direct mode.
  */
 
 #include <cmath>
@@ -41,13 +42,19 @@ int main()
   /* Start configuration */
 
   // Modem modulation
-  const int modem_mod_type_reg = RF4463_TX_DIRECT_MODE_TYPE_SYNCHRONOUS |
-                                RF4463_TX_DIRECT_MODE_GPIO0 |
-                                RF4463_MOD_SOURCE_PACKET_HANDLER |
-                                RF4463_MOD_TYPE_2FSK;
+  const int modem_mod_type_reg = RF4463_TX_DIRECT_MODE_TYPE_ASYNCHRONOUS |
+                                 RF4463_TX_DIRECT_MODE_GPIO1 |
+                                 RF4463_MOD_SOURCE_DIRECT_MODE |
+                                 RF4463_MOD_TYPE_OOK;
   // CW, OOK, FSK, GFSK
   const float rf_freq = 915.0; // MHz
   const float freq_xo = 30.0; // MHz
+
+  // PA: CW, OOK, FSK, GFSK
+  const uint8_t pa_mode = RF4463_PA_MODE_CLASS_E | RF4463_PA_MODE_4_GROUPS;
+  const uint8_t pa_level = 127; // 0-127
+  const uint8_t pa_clock_duty_cycle = 0; // Only switched current mode
+  const uint8_t pa_output_bias = 0; // 0-63
 
   // OOK, FSK, GFSK
   const float data_rate = 123.0; // kbps
@@ -57,18 +64,15 @@ int main()
   const uint8_t ramp_time = RF4463_PA_TC_10_00_US;
   const uint8_t pa_to_mod_delay = RF4463_PA_TC_FSK_MOD_DLY_10_US;
 
-  // PA: CW, OOK, FSK, GFSK
-  const uint8_t pa_mode = RF4463_PA_MODE_CLASS_E | RF4463_PA_MODE_4_GROUPS;
-  const uint8_t pa_level = 127; // 0-127
-  const uint8_t pa_clock_duty_cycle = 0; // Only switched current mode
-  const uint8_t pa_output_bias = 0; // 0-63
-
   // Bit mapping: OOK, FSK, GFSK
   const int modem_map_control_reg = RF4463_MODEM_MAP_CONTROL_ENMANCH
                                     | (~RF4463_MODEM_MAP_CONTROL_ENMANCH & 0)
                                     & ~RF4463_MODEM_MAP_CONTROL_ENINV_RXBIT
                                     & ~RF4463_MODEM_MAP_CONTROL_ENINV_TXBIT
                                     & ~RF4463_MODEM_MAP_CONTROL_ENINV_FD;
+
+  // Crystal clock configuration
+  const int global_xo_tune = 0x40;
 
   /* Please do not edit anything beyond this point */
 
@@ -80,21 +84,58 @@ int main()
   uint32_t reg_fd = 524288.0f * (double)outdiv * freq_dev * 1000 / (2 * freq_xo * 1000000);
   uint8_t reg_pa_tc = 20.0f * 0/ (32.0 - ramp_time) + 0x1D;
 
+  // Crystal clock configuration
+  std::cout << "GLOBAL_XO_TUNE = " << std::hex << std::uppercase << "0x" << global_xo_tune << std::endl;
+  std::cout << "GLOBAL_CLK_CFG = " << std::hex << std::uppercase << "0x" << global_xo_tune << std::endl;
+  std::cout << std::endl;
+
   // Frequency configuration
   std::cout << "FREQ_CONTROL_INTE = " << std::hex << std::uppercase << "0x" << fc_inte << std::endl;
   std::cout << "FREQ_CONTROL_FRAC_2/1/0 = " << std::hex << std::uppercase << "0x" << (fc_frac & 0xFFFFFF) << std::endl;
+  std::cout << std::endl;
 
   // Modem configuration
-  std::cout << "MODEM_FREQ_DEV_2/1/0 = " << std::hex << std::uppercase << "0x" << ((uint32_t)(reg_fd) & 0xFFFFFF) << std::endl;
+  std::cout << "MODEM_FREQ_DEV_2/1/0 = " << std::hex << std::uppercase << "0x" << ((reg_fd) & 0xFFFFFF) << std::endl;
   std::cout << "MODEM_MOD_TYPE = " << std::hex << std::uppercase << "0x" << modem_mod_type_reg << std::endl;
   std::cout << "MODEM_MAP_CONTROL = " << std::hex << std::uppercase << "0x" << modem_map_control_reg << std::endl;
   std::cout << "MODEM_DATA_RATE_2/1/0 = " << std::hex << std::uppercase << "0x" << (reg_dr & 0xFFFFFF) << std::endl;
+  std::cout << std::endl;
 
   // Power amplifier
-  std::cout << "PA_MODE = " << std::hex << std::uppercase << "0x" << unsigned(pa_mode)<< std::endl;
-  std::cout << "PA_PWE_LVL = " << std::hex << std::uppercase << "0x" << unsigned(pa_level)<< std::endl;
+  std::cout << "PA_MODE = " << std::hex << std::uppercase << "0x" << unsigned(pa_mode) << std::endl;
+  std::cout << "PA_PWE_LVL = " << std::hex << std::uppercase << "0x" << unsigned(pa_level) << std::endl;
   std::cout << "PA_BIAS_CLKDUTY = " << std::hex << std::uppercase << "0x" << 0 << std::endl;
   std::cout << "PA_TC = " << std::hex << std::uppercase << "0x" << (reg_pa_tc | (pa_to_mod_delay << 5)) << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "const uint8_t power_amp_config[] = {0x" << std::hex << std::uppercase
+            << unsigned(pa_mode) <<  ", 0x"
+            << unsigned(pa_level) <<  ", 0x"
+            << (reg_pa_tc | (pa_to_mod_delay << 5))
+            << "};" << std::endl;
+  std::cout << "set_properties(Si446x_PROP_PA_MODE, power_amp_config, sizeof(power_amp_config));" << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "const uint8_t modem_config[] = {0x" << std::hex << std::uppercase
+            << ((reg_fd >> 16) & 0xFF) << ", 0x"
+            << ((reg_fd >> 8) & 0xFF) << ", 0x"
+            << (reg_fd & 0xFF) << ", 0x"
+            << modem_mod_type_reg << ", 0x"
+            << modem_map_control_reg << ", 0x"
+            << ((reg_dr >> 16) & 0xFF) << ", 0x"
+            << ((reg_dr >> 8) & 0xFF) <<  ", 0x"
+            << (reg_dr & 0xFF)
+            << "};" << std::endl;
+  std::cout << "set_properties(Si446x_PROP_MODEM_FREQ_DEV_2, modem_config, sizeof(modem_config));" << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "const uint8_t freq_config[] = {0x" << std::hex << std::uppercase
+            << fc_inte << ", 0x"
+            << ((fc_frac >> 16) & 0xFF) << ", 0x"
+            << ((fc_frac >> 8) & 0xFF) <<  ", 0x"
+            << (fc_frac & 0xFF)
+            << "};" << std::endl;
+  std::cout << "set_properties(Si446x_PROP_FREQ_CONTROL_INTE, freq_config, sizeof(freq_config));" << std::endl;
 
   return 0;
 }

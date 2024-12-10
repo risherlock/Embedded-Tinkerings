ed3966aa9dcb3e7e916c165bfdeb078b3d9695b3
risherlock <risherlock221b@gmail.com>
2024-12-10 17:16:44 +0100

    change pins for working tx

--- drivers/stm32f103/libs/si446x/si446x_hal.c
+++ drivers/stm32f103/libs/si446x/si446x_hal.c
@@ -11,63 +11,64 @@ void si446x_hal_init(void)
 
 void si446x_hal_morse_init(void)
 {
-  // GPIO1: BP12
+  // GPIO1: PB0
   RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-  GPIOB->CRH &= ~(GPIO_CRH_MODE12 | GPIO_CRH_CNF12);
-  GPIOB->CRH |= GPIO_CRH_MODE12_0;
+  GPIOB->CRL &= ~(GPIO_CRL_MODE0 | GPIO_CRL_CNF0);
+  GPIOB->CRL |= GPIO_CRL_MODE0_0;
 }
 
 void si446x_hal_spi_init(void)
 {
-  // NSS
-  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
-  GPIOA->CRL |= GPIO_CRL_MODE4;
-  GPIOA->CRL &= ~GPIO_CRL_CNF4;
-  GPIOA->BSRR |= GPIO_BSRR_BS4;
+  // NSS: PB12
+  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN | RCC_APB2ENR_AFIOEN;
+  GPIOB->CRH |= GPIO_CRH_MODE12;
+  GPIOB->CRH &= ~GPIO_CRH_CNF12;
+  GPIOB->BSRR |= GPIO_BSRR_BS12;
 
-  // SCK
-  GPIOA->CRL &= ~GPIO_CRL_CNF5;
-  GPIOA->CRL |= GPIO_CRL_MODE5 | GPIO_CRL_CNF5_1;
+  // SCK: PB13
+  GPIOB->CRH &= ~GPIO_CRH_CNF13;
+  GPIOB->CRH |= GPIO_CRH_MODE13 | GPIO_CRH_CNF13_1;
 
-  // MISO
-  GPIOA->CRL &= ~(GPIO_CRL_MODE6 | GPIO_CRL_CNF6);
-  GPIOA->CRL |= GPIO_CRL_CNF6_0;
+  // MISO: PB14
+  GPIOB->CRH &= ~(GPIO_CRH_MODE14 | GPIO_CRH_CNF14);
+  GPIOB->CRH |= GPIO_CRH_CNF14_0;
 
-  // MOSI
-  GPIOA->CRL &= ~GPIO_CRL_CNF7;
-  GPIOA->CRL |= (GPIO_CRL_MODE7 | GPIO_CRL_CNF7_1);
+  // MOSI: PB15
+  GPIOB->CRH &= ~GPIO_CRH_CNF15;
+  GPIOB->CRH |= (GPIO_CRH_MODE15 | GPIO_CRH_CNF15_1);
 
-  // SPI
-  RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-  SPI1->CR1 = 0;
-  SPI1->CR1 |= SPI_CR1_SSM |
+  // SPI2
+  RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
+  SPI2->CR1 = 0;
+  SPI2->CR1 |= SPI_CR1_SSM |
                SPI_CR1_SSI |
                SPI_CR1_BR_2 |
                SPI_CR1_MSTR;
 
-  SPI1->CR1 |= SPI_CR1_SPE;
+  SPI2->CR1 |= SPI_CR1_SPE;
 }
 
+// PA7
 void si446x_hal_sdn_init(void)
 {
-  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-  GPIOB->CRL &= ~(GPIO_CRL_CNF6 | GPIO_CRL_MODE6);
-  GPIOB->CRL |= GPIO_CRL_MODE6_1;
+  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+  GPIOA->CRL &= ~(GPIO_CRL_CNF7 | GPIO_CRL_MODE7);
+  GPIOA->CRL |= GPIO_CRL_MODE7_1;
 }
 
 void si446x_hal_spi_write_byte(const uint8_t cmd)
 {
-  while (!(SPI1->SR & SPI_SR_TXE));
-  SPI1->DR = 0x00ff & cmd;
-  while((SPI1->SR & SPI_SR_BSY));
+  while (!(SPI2->SR & SPI_SR_TXE));
+  SPI2->DR = 0x00ff & cmd;
+  while(SPI2->SR & SPI_SR_BSY);
 }
 
 uint8_t si446x_hal_spi_read_byte(const uint8_t cmd)
 {
   si446x_hal_spi_write_byte(cmd);
-  while(!(SPI1->SR & SPI_SR_RXNE));
-  uint8_t output = SPI1->DR;
-  while((SPI1->SR & SPI_SR_BSY));
+  while(!(SPI2->SR & SPI_SR_RXNE));
+  uint8_t output = SPI2->DR;
+  while(SPI2->SR & SPI_SR_BSY);
   return output;
 }
 
